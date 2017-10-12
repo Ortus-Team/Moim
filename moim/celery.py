6f8796a1d4ff3d219da20f6855de023d80c81c83
Ahhyun Cho <ahhyun01@cs.washington.edu>
2017-10-11 23:07:03 -0700

    modify configs

--- moim/celery.py
+++ moim/celery.py
@@ -9,7 +9,7 @@ from django.apps import apps
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moim.settings.local_base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moim.settings.local")
 
 app = Celery('moim_tasks')
 app.config_from_object('django.conf:settings', namespace='CELERY')
