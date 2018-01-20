6d250043dc938b51d2843d30bbe33e89841a1c9f
Ahhyun Cho <ahhyun01@cs.washington.edu>
2018-01-19 16:01:43 -0800

    Add base html page

--- moim/urls.py
+++ moim/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
+from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 
 import django_js_reverse.views
@@ -17,4 +18,5 @@ urlpatterns = [
     url(r'^api/v1/', include('event.urls', namespace='events')),
     url(r'^api/v1/', include('category.urls', namespace='categories')),
     url(r'^api/v1/', include('tag.urls', namespace='tags')),
+    url(r'^$', TemplateView.as_view(template_name='exampleapp/itworks.html'), name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
